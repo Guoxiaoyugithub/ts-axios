0b0206b4afbb6649ca15789c3e32c66fbc0568b5
guowy <133698099@qq.com>
2020-04-16 17:47:19 +0800

    add error

--- example/base/app.ts
+++ example/base/app.ts
@@ -1,5 +1,7 @@
 import axios from '../../src/index'
-
+/**
+ * 测试url的测试用例
+ */
 // axios({
 //   method: 'get',
 //   url: '/base/get',
@@ -60,6 +62,61 @@ import axios from '../../src/index'
 //     bar: 'baz'
 //   }
 // })
+
+/**
+ * 测试data的测试用例
+ */
+// axios({
+//   method: 'post',
+//   url: '/base/post',
+//   data: {
+//     a: 1,
+//     b: 2
+//   }
+// })
+//
+// const arr = new Int32Array([21, 31])
+//
+// axios({
+//   method: 'post',
+//   url: '/base/buffer',
+//   data: arr
+// })
+// axios({
+//   method: 'post',
+//   url: '/base/post',
+//   data: {
+//     a: 1,
+//     b: 2
+//   }
+// })
+/**
+ * 测试headers的测试用例
+ */
+// axios({
+//   method: 'post',
+//   url: '/base/post',
+//   headers: {
+//     'content-type': 'application/json;'
+//   },
+//   data: {
+//     a: 1,
+//     b: 2
+//   }
+// })
+//
+// const paramsString = 'q=URLUtils.searchParams&topic=api'
+// const searchParams = new URLSearchParams(paramsString)
+//
+// axios({
+//   method: 'post',
+//   url: '/base/post',
+//   data: searchParams
+// })
+
+/**
+ * promise化的测试用例
+ */
 axios({
   method: 'post',
   url: '/base/post',
@@ -67,12 +124,18 @@ axios({
     a: 1,
     b: 2
   }
+}).then((res) => {
+  console.log(res)
 })
-
-const arr = new Int32Array([21, 31])
 
 axios({
   method: 'post',
-  url: '/base/buffer',
-  data: arr
+  url: '/base/post',
+  responseType: 'json',
+  data: {
+    a: 3,
+    b: 4
+  }
+}).then((res) => {
+  console.log(res)
 })
--- src/helpers/data.ts
+++ src/helpers/data.ts
@@ -1,8 +1,25 @@
 import { isPlainObject } from './util'
-
+import { cat } from 'shelljs'
+/**
+ * transformRequest函数接收index.ts传来的AxiosRequestConfig类型中的data属性
+ * data如果是普通的对象(PlainObject),直接转化成JSON字符串
+ * datar如果不是普通对象，比如流文件，就直接返回该类型
+ */
 export function transformRequest(data: any): any {
+
   if (isPlainObject(data)) {
     return JSON.stringify(data)
+  }
+  return data
+}
+
+export  function transformRespons(data:any):any {
+  if(typeof data === 'string'){
+    try {
+      data = JSON.parse(data)
+    }catch(e){
+      // do nothing
+    }
   }
   return data
 }
