a2a9cab8bd0eda9efc6300417d9b31d643f26650
guowy <133698099@qq.com>
2020-04-22 21:48:03 +0800

    修复了axios cancel无法取消请求的问题

--- src/core/xhr.ts
+++ src/core/xhr.ts
@@ -4,7 +4,7 @@ import { createError} from '../helpers/error'
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
   return new Promise((resolve,reject)=>{
-    const { url, method = 'get', data = null ,headers,responseType,timeout} = config
+    const { url, method = 'get', data = null ,headers,responseType,timeout,cancelToken} = config
     const request = new XMLHttpRequest()
     if(responseType){
       request.responseType = responseType
@@ -50,6 +50,14 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
         request.setRequestHeader(name,headers[name])
       }
     })
+
+    if(cancelToken){
+      cancelToken.promise.then(reason=>{
+        request.abort()
+        console.log("request.abort")
+        reject(reason)
+      })
+    }
 
     request.send(data)
 
